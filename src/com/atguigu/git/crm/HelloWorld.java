34eb0094e37515c58ca078d0abba8bbc25bb701f
glt19941003 <glt19941003@163.com>
2016-07-08 21:32:58 +0800

    Update HelloWorld.java

--- src/com/atguigu/git/crm/HelloWorld.java
+++ src/com/atguigu/git/crm/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		System.out.println("Hello World!!");
 		System.out.println("update 02");
+		System.out.println("update 03");
 	}
 	
 }
